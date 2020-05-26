c365df460f9192713cb75c8af5e816b2709a4aa1
Efren Vasquez <e.vasquez@ufl.edu>
2020-05-26 16:49:25 -0400

    Creates related posts section

--- includes/class-news-website-template-loader.php
+++ includes/class-news-website-template-loader.php
@@ -357,3 +357,58 @@ if( function_exists('acf_add_local_field_group') ):
 	));
 
 	endif;
+
+/*========================================
+
+	Related Posts
+
+=========================================*/
+	function relatedPosts(){
+		?>
+		<div class="wrap related-posts">
+			<section class="relatedposts">
+				<h3>Related posts</h3>
+				<div class="relatedposts-container">
+					<?php
+						global $post;
+						$tags = wp_get_post_tags($post->ID);
+
+						//Gets arguments for Related Posts Query
+						if ($tags) {
+							$tag_ids = array();
+							foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;
+								$args=array(
+								'tag__in' => $tag_ids,
+								'post__not_in' => array($post->ID),
+								'posts_per_page'=>4, // Number of related posts to display.
+								'ignore_sticky_posts'=>1
+							);
+
+							$my_query = new wp_query( $args );
+
+							while( $my_query->have_posts() ) {
+								$my_query->the_post();
+							?>
+
+							<div class="relatedthumb">
+								<a rel="external" href="<? the_permalink()?>">
+								<div class="related-post-featured-image">
+									<?php the_post_thumbnail('thumbnail'); ?>
+								</div>
+								<div class="related-post-title-excerpt">
+									<h4><?php echo get_the_title(); ?></h4>
+									<p><?php echo get_the_excerpt(); ?></p>
+								</div>
+								</a>
+							</div>
+
+							<? }
+						}
+						wp_reset_query();
+						?>
+					</div> <!-- relatedposts-container -->
+			</section>
+		</div> <!-- Wrap -->
+		<?php
+	}
+
