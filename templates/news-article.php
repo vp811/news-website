c516cbc9ca39b2640afc0454230a4d6f37e7973f
Efren Vasquez <e.vasquez@ufl.edu>
2020-06-01 17:18:02 -0400

    sets up news article template

--- templates/news-article.php
+++ templates/news-article.php
@@ -1,20 +1,70 @@
 <?php
-/* Template Name: News Article */
+/* Template Name: News Article
+*/
   get_header();
+
+  global $post;
+  $author_id = $post->post_author;
 ?>
-<style>
-</style>
-<div class="wrap">
 
   <div id="primary" class="content-area">
     <main id="main" class="site-main">
-      <div class="featured-news top-section">
-        <?php the_post_thumbnail(); ?>
-      </div>
-      <h2 class="entry-title"><?php echo get_the_title(); ?></h2>
+      <?php if(have_posts()){
+        while (have_posts()) {
+          the_post();
+
+          //Pulls in the Author's first and last name along with the publish date
+          $authorFirstName = get_the_author_meta('first_name');
+          $authorLastName  = get_the_author_meta('last_name');
+          $publishDate     = get_the_date();
+          ?>
+          <div class="news-article top-section">
+            <?php the_post_thumbnail();
+
+            // Displays image caption if there is one
+              $get_description = get_post(get_post_thumbnail_id())->post_excerpt;
+
+              if(!empty($get_description)){//If description is not empty show the div
+                echo '<div class="featured_caption"><p>' . $get_description . '</p></div>';
+              }
+            ?>
+          </div>
+            <div class="wrap">
+              <div class="article-intro">
+                <h2><?php echo get_the_title(); ?></h2>
+
+                <!-- Displays subheading if one is set -->
+                <?php if(!empty(get_field('subheading'))){
+                  echo "<p class='sub-heading'>" . get_field('subheading') . "</p>";
+                } ?>
+
+                <!-- Displays author's first name and last name and the publish date -->
+                <p class="byline-date"><?php echo "By <span class='author-name'>$authorFirstName $authorLastName</span> — <span class='publish-date'>$publishDate</span>";?></p>
+              </div>
+
+
+
+            <div class="body-content">
+              <?php the_content(); ?>
+            </div>
+          </div><!-- Wrap -->
+        <?php }
+      }
+
+        //Display Share Buttons
+        socialMedia();
+
+        //Displays the support text and button
+        supportText();
+
+        echo "<hr />";
+
+        //Displays the related posts
+        relatedPosts();
+      ?>
+
     </main><!-- #main -->
   </div><!-- #primary -->
-</div><!-- .wrap -->
 
 <?php
   get_footer();
