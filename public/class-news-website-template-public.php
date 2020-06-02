2da2c9a203cd682c942dc57ab848bdc75d80c33f
Efren Vasquez <e.vasquez@ufl.edu>
2020-06-02 16:53:55 -0400

    add comments to css if statements

--- public/class-news-website-template-public.php
+++ public/class-news-website-template-public.php
@@ -78,8 +78,14 @@ class News_Website_Template_Public {
 			wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/news-website-feature-article-template-public.css', array(), $this->version, 'all' );
 		}
 
+		//If the page template is the News Article, this CSS will load
 		if ( is_page_template( '../templates/news-article.php' ) ) {
 			wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/news-website-article-template-public.css', array(), $this->version, 'all' );
+		}
+
+		//If the page template is the home page, this CSS will load
+		if ( is_page_template( '../templates/news-home-page.php' ) ) {
+			wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/news-website-home-template-public.css', array(), $this->version, 'all' );
 		}
 	}
 
